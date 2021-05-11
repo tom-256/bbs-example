53cd69930a4b4aafccbfe627ca11f055430d6e1a
tom-256 <t11a.256.512.1024@gmail.com>
2021-05-11 20:18:19 +0900

    add comment

--- src/App.tsx
+++ src/App.tsx
@@ -24,48 +24,48 @@ type PostProps = {
 const postsMock: PostProps[] = [
   {
     id: "1",
-    title: "title1",
-    description: "test description",
+    title: "タイトル1",
+    description: "説明",
     author: "bob",
     image:
       "https://cdn.shibe.online/shibes/5464189a1ded1107e6579b8f39e82937acd37554.jpg",
-    tags: ["tag1", "tag2"],
+    tags: ["タグ1", "タグ2"],
   },
   {
     id: "2",
-    title: "title2",
-    description: "test description",
+    title: "タイトル2",
+    description: "説明",
     author: "john",
     image:
       "https://cdn.shibe.online/shibes/f516c9b186ea3997cf42b0257a9e2dc031af90cd.jpg",
-    tags: ["tag3", "tag4"],
+    tags: ["タグ3", "タグ4"],
   },
   {
     id: "3",
-    title: "title3",
-    description: "test description",
+    title: "タイトル3",
+    description: "説明",
     author: "bob",
     image:
       "https://cdn.shibe.online/shibes/9e77314c58f019b3c845dbb4c92f329114595def.jpg",
-    tags: ["tag5", "tag6"],
+    tags: ["タグ5", "タグ6"],
   },
   {
     id: "4",
-    title: "title4",
-    description: "test description",
+    title: "タイトル4",
+    description: "説明",
     author: "john",
     image:
       "https://cdn.shibe.online/shibes/bb3dc550e82c9803caec7d3378b775baa18f5e9a.jpg",
-    tags: ["tag7", "tag8"],
+    tags: ["タグ7", "タグ8"],
   },
   {
     id: "5",
-    title: "title5",
-    description: "test description",
+    title: "タイトル5",
+    description: "説明",
     author: "john",
     image:
       "https://cdn.shibe.online/shibes/c608636ae06a2a6123f76e4497fe7503593cd52a.jpg",
-    tags: ["tag9", "tag10"],
+    tags: ["タグ9", "タグ10"],
   },
 ];
 
@@ -138,6 +138,7 @@ const Board: VFC<BoardProps> = ({ posts }) => {
                     width="50px"
                   ></img>
                   <div>{post.author}</div>
+                  <div>{post.tags}</div>
                 </div>
               )}
             </Draggable>
