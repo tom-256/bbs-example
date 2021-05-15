f81bb6c73a563d1ad511e0029762b1c362ce0ffc
tom-256 <t11a.256.512.1024@gmail.com>
2021-05-15 10:37:29 +0900

    remove dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
 import { useState, VFC } from "react";
 import "./App.css";
-import {
-  DragDropContext,
-  Draggable,
-  DraggableLocation,
-  DraggableProvided,
-  DraggableStateSnapshot,
-  Droppable,
-  DroppableProvided,
-  DroppableStateSnapshot,
-  DropResult,
-} from "react-beautiful-dnd";
 
 type PostProps = {
   id: string;
@@ -69,102 +58,29 @@ const postsMock: PostProps[] = [
   },
 ];
 
-const reorderPosts = (
-  posts: PostProps[],
-  source: DraggableLocation,
-  dest: DraggableLocation
-): PostProps[] => {
-  const result = Array.from(posts);
-  const [removed] = result.splice(source.index, 1);
-  result.splice(dest.index, 0, removed);
-  return result;
-};
-
-const grid: number = 8;
-
-const getTaskItemStyle = (draggableStyle: any, isDragging: boolean): {} => ({
-  userSelect: "none",
-  padding: 2 * grid,
-  margin: `0 0 ${grid}px 0`,
-  background: isDragging ? "lightgreen" : "grey",
-  ...draggableStyle,
-  width: "100px",
-});
-
-const getBoardStyle = (isDraggingOver: boolean) => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: grid,
-  display: "flex",
-  alignItems: "start",
-  width: "1000px",
-  minWidth: "1000px",
-  minHeight: "100px",
-});
-
 type BoardProps = {
   posts: PostProps[];
 };
 
 const Board: VFC<BoardProps> = ({ posts }) => {
   return (
-    <Droppable droppableId={"BSS"} direction="horizontal">
-      {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.droppableProps}
-          style={getBoardStyle(snapshot.isDraggingOver)}
-        >
-          {posts.map((post, index) => (
-            <Draggable draggableId={post.id} index={index} key={post.id}>
-              {(
-                provided: DraggableProvided,
-                snapshot: DraggableStateSnapshot
-              ) => (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.draggableProps}
-                  {...provided.dragHandleProps}
-                  style={getTaskItemStyle(
-                    provided.draggableProps.style,
-                    snapshot.isDragging
-                  )}
-                >
-                  <div>{post.title}</div>
-                  <div>{post.description}</div>
-                  <img
-                    src={post.image}
-                    alt="test"
-                    height="50px"
-                    width="50px"
-                  ></img>
-                  <div>{post.author}</div>
-                  <div>{post.tags}</div>
-                </div>
-              )}
-            </Draggable>
-          ))}
-          {provided.placeholder}
+    <div>
+      {posts.map((post, index) => (
+        <div>
+          <div>{post.title}</div>
+          <div>{post.description}</div>
+          <img src={post.image} alt="test" height="50px" width="50px"></img>
+          <div>{post.author}</div>
+          <div>{post.tags}</div>
         </div>
-      )}
-    </Droppable>
+      ))}
+    </div>
   );
 };
 
 const App = () => {
   const [posts, setPosts] = useState<PostProps[]>(postsMock);
   const [searchQuery, setQuery] = useState<string>("");
-
-  function onDragEnd(result: DropResult) {
-    const { source, destination: dest } = result;
-
-    if (!dest) return;
-    const { droppableId: sourceId } = source;
-    const { droppableId: destinationId } = dest;
-
-    if (sourceId !== destinationId) return;
-    const ordered = reorderPosts(posts, source, dest);
-    setPosts(ordered);
-  }
 
   const filterPosts = (query: string): PostProps[] => {
     if (query === "") return postsMock;
@@ -192,9 +108,7 @@ const App = () => {
           }}
           placeholder={`search`}
         />
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Board posts={searchQuery ? filterPosts(searchQuery) : posts}></Board>
-        </DragDropContext>
+        <Board posts={searchQuery ? filterPosts(searchQuery) : posts}></Board>
       </div>
     </div>
   );
